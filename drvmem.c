74cd5469aa2ebab095eb5eb87e4e6ba14b2858b3
Siirko <alexander.yanovskyy@gmail.com>
2023-11-25 14:01:34 +0100

    /dev/null implemented

--- drvmem.c
+++ drvmem.c
@@ -15,13 +15,41 @@
 int 
 drvmemread(struct inode *ip, char *dst, int n, uint off)
 {
+    if(ip->major == DNULL)
+    {
+        switch (ip->minor)
+        {
+           case 0:
+                return 0;
+            case 1:
+                memset(dst, 0, n);
+                return n;
+            case 2:
+                if (off < EXTMEM || off + n > PHYSTOP)
+                    return -1;
+            default:
+                return -1;
+        }
+    }
     return 0;
 }
 
 int 
 drvmemwrite(struct inode *ip, char *buf, int n, uint off)
 {
-    return 0;
+    if (ip->major == 2)
+    {
+        switch (ip->minor)
+        {
+            case 0:
+                return n;
+            case 1:
+                return n;
+            default:
+                return -1;
+        }
+    }
+    return -1;
 }
 
 void
@@ -29,4 +57,6 @@ drvmeminit(void)
 {
     devsw[DNULL].read = drvmemread;
     devsw[DNULL].write = drvmemwrite;
+
+    // ioapicenable(IRQ_KBD, 0);
 }
