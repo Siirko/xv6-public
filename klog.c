d1f7736ca9f74c8ad361c7707adde6ab0dd66ccd
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2023-12-01 12:00:03 +0100

    read and write klog

--- klog.c
+++ klog.c
@@ -13,20 +13,80 @@
 #include "proc.h"
 #include "x86.h"
 
+struct spinlock klogloc;
+
+#define NEVENTS 16 // total of events
+
+struct logev logev[NEVENTS] = {0};
+uint logevempty;
+
 int 
 klogread(struct inode *ip, char *dst, int n)
 {
+    if(ip->major == KLOG)
+    {
+        ilock(ip);
+
+        uint total_asked = n / sizeof(struct logev);
+        if (total_asked == 0)
+        {
+            iunlock(ip);
+            return 0;
+        }
+
+        // if buffer is empty, wait until one event appeared
+        // active wating, didn't know how to do in another way at this moment
+        // needs to be changed !
+        while(logevempty == 0)
+            ;
+        
+        for(uint i = 0; i < total_asked; i++)
+            memmove(&logev[i % NEVENTS], dst, sizeof(struct logev));
+            
+        iunlock(ip);
+        return sizeof(struct logev) * total_asked;
+    }
     return 0;
 }
 int 
 klogwrite(struct inode *ip, char *buf, int n)
 {
+    if(ip->major == KLOG)
+    {
+        ilock(ip);
+        acquire(&klogloc);
+        uint total_asked = n / sizeof(struct logev);
+        if (n == 0)
+        {
+            iunlock(ip);
+            return 0;
+        }
+        
+        // dont handle when user ask not a multiple of sizeof logev
+
+        for(uint i = 0; i < total_asked; i++)
+        {
+            struct logev curr_log = {0};
+            memmove(&curr_log, buf, sizeof(int));
+            memmove(&curr_log, buf + sizeof(int), MAX_SENDER);
+            memmove(&curr_log, buf + sizeof(int) + MAX_SENDER, MAX_EVENT);
+            memmove(&logev[i % NEVENTS], &curr_log, sizeof(struct logev));
+            if(logevempty == 0)
+                logevempty = 1;
+        }
+
+        release(&klogloc);
+        iunlock(ip);
+        return n;
+    }
     return 0;
 }
 
 void
 kloginit(void)
 {
+    // initlock(&klogloc, "klog");
     devsw[KLOG].read = klogread;
     devsw[KLOG].write = klogwrite;
+    logevempty = 0;
 }
